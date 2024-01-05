b1e9ec6600c30fda330cf1b21d94ea8638bfcec9
Jeff Sisson <jeff.sisson@nytimes.com>
2024-01-05 12:58:59 -0500

    fix background process termination

--- benchmark-go.sh
+++ benchmark-go.sh
@@ -1,6 +1,8 @@
 npm run build-go-js
-go run server-go.go &
+go build -o go-server-binary server-go.go
+./go-server-binary &
 pid=$!
-curl --head -X GET --retry 5 --retry-connrefused --retry-delay 1 "http://localhost:8080" > /dev/null 2>&1
+curl --head -X GET --retry 10 --retry-connrefused --retry-delay 5 "http://localhost:8080" > /dev/null 2>&1
 echo "server ready"
 k6 run --vus 40 --duration 30s k6.js
+kill $pid
--- benchmark-node.sh
+++ benchmark-node.sh
@@ -1,5 +1,6 @@
 node server-node.js &
 pid=$!
-curl --head -X GET --retry 5 --retry-connrefused --retry-delay 1 "http://localhost:8080" 2>&1 /dev/null
+curl --head -X GET --retry 5 --retry-connrefused --retry-delay 1 "http://localhost:8080" > /dev/null 2>&1
 echo "server ready"
 k6 run --vus 40 --duration 30s k6.js
+kill $pid
